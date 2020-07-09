30f0075e0ed9c015c5e17c1a2f3470bf053af6e1
pablo <pablo@codice.es>
2020-07-09 13:57:41 +0000

    Initial commit plus a fix
    A fix was needed in https://github.com/microsoft/VFSForGit/issues/1681#issuecomment-656106722

---
+++ MirrorProvider.Windows/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace MirrorProvider.Windows
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // enable projfs: as root in powershell: Enable-WindowsOptionalFeature -Online -FeatureName Client-ProjFS -NoRestart 
+
+            MirrorProviderCLI.Run(args, new WindowsFileSystemVirtualizer());
+        }
+    }
+}
